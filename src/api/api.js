f64d306bad4079a7a3d6557fd9e383d23b7b8c75
preetisharma-ACL <preetiaajneeti@gmail.com>
2026-04-25 16:16:04 +0530

    update

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const BASE_URL = "https://metadashboard.aajneeticonnectltd.com/api";
 
 export async function api(endpoint, options = {}) {
   const auth = JSON.parse(localStorage.getItem("auth"));
