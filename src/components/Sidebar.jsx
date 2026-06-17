6dd9c1eed03ccfb388682c4e49afc6a52bd5dfdb
preetisharma-ACL <preetiaajneeti@gmail.com>
2026-06-17 11:24:28 +0530

    active performing project

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -212,6 +212,11 @@ export default function Sidebar() {
               <SmallIcon d="M9 17v-2m3 2v-4m3 4v-6m2 10H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
             ),
           },
+          {
+            name: "Performing Projects",
+            path: "/performing-projects",
+            icon: () => <Icon d="M13 10V3L4 14h7v7l9-11h-7z" />,
+          },
         ],
       },
       // {
@@ -271,12 +276,12 @@ export default function Sidebar() {
       //   icon: () => <Icon d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />,
       //   path: "/leads-performance",
       // },
-      // {
-      //   name: "Performing Projects",
-      //   roles: ["admin"],
-      //   icon: () => <Icon d="M13 10V3L4 14h7v7l9-11h-7z" />,
-      //   path: "/performing-projects",
-      // },
+      {
+        name: "Performing Projects",
+        roles: ["admin"],
+        icon: () => <Icon d="M13 10V3L4 14h7v7l9-11h-7z" />,
+        path: "/performing-projects",
+      },
       // {
       //   name: "Leads",
       //   roles: ["admin"],
