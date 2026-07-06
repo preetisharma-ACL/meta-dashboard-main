d25be77555cfd8c2b809feb6c6bd5c6deec8f0e9
preetisharma-ACL <preetiaajneeti@gmail.com>
2026-07-06 12:59:09 +0530

    fix issues in sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -229,6 +229,7 @@ export default function Sidebar() {
           {
             name: "Clients",
             path: "/clients",
+            roles: ["admin", "campaign_manager"],
             icon: () => (
               <SmallIcon d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
             ),
@@ -236,6 +237,7 @@ export default function Sidebar() {
           {
             name: "Client Nomen",
             path: "/client-nomen",
+            roles: ["admin", "campaign_manager"],
             icon: () => (
               <SmallIcon d="M7 7h.01M7 3h5c.512 0 1.024.195 1.414.586l7 7a2 2 0 010 2.828l-7 7a2 2 0 01-2.828 0l-7-7A1.994 1.994 0 013 12V7a4 4 0 014-4z" />
             ),
@@ -243,6 +245,7 @@ export default function Sidebar() {
           {
             name: "Project Display Config",
             path: "/project-display-config",
+            roles: ["admin", "campaign_manager"],
             icon: () => (
               <SmallIcon d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4" />
             ),
@@ -250,6 +253,7 @@ export default function Sidebar() {
           {
             name: "Leed Feeding",
             path: "/feeded-leads",
+            roles: ["admin", "campaign_manager"],
             icon: () => (
               <SmallIcon d="M9 17v-2m3 2v-4m3 4v-6m2 10H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
             ),
